88895c1f08404bdccc4f1e7c6d1937eca024a6a4
ntutech <57479466+ntutech@users.noreply.github.com>
2020-12-28 16:49:18 +0800

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,8 +1,7 @@
-(function() {
+
     alert("hello");
     'use strict';
            if (window.location.href.indexOf('SAMLRequest') > 0) {
                 console.log("Redirect to LibCal");
 				setTimeout(function(){ window.location.href = "https://libcalendar.ntu.edu.sg/"; }, 3000);
             }
-})();
